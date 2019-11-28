a5a5cf59419c18589ebaa3f4129b6218bdefa023
Tobias Rosskopf <t.rosskopf@bgswasser.de>
2019-11-28 15:32:02 +0100

    Updated test_kivycalculator.py

---
+++ tests/test_kivy_calculator.py
@@ -0,0 +1,32 @@
+#! python3.8
+# -*- coding: utf-8 -*-
+
+# File name:    test_kivy_calculator.py
+# Author:       Tobias Rosskopf
+# Email:        tobirosskopf@gmail.com
+# Created:      28.11.2019
+# Modified:     28.11.2019
+
+"""
+Unittest for module kivy_calculator.py.
+"""
+
+# Standard imports
+import unittest
+
+# Package imports
+import kivy_calculator.kivy_calculator
+
+
+class TestKivyCalculator(unittest.TestCase):
+    """TODO: Testclass's docstring"""
+
+    def test_eval(self):
+        """TODO: Test's docstring"""
+        query = "134 * 5 / 7"
+        solution = "24.285714285714285"
+        self.assertEqual(str(eval(query)), solution)
+
+
+if __name__ == '__main__':
+    unittest.main()
